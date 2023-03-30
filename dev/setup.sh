f37ffae9f9594301665801e55ca27c6db720bcd5
skelly37 <skelly37@protonmail.com>
2023-03-30 23:30:57 +0200

    removed redundancy from the setup

--- dev/setup.sh
+++ dev/setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 python3 -m pip install -r dev/requirements-dev.txt || echo "Could not install the Python requirements. Check your Python & pip installation and be sure that you are in the repository root." && exit 1
-pip3 install pre-commit && pre-commit install
+pre-commit install
